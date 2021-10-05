1f635c37ffc5eb4397f5f833122b4d091a193562
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-10-04 22:37:30 -0300

    Criando teste de createUserUseCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -13,7 +13,7 @@ let addressProvider: AddressProvider;
 
 describe("Create user", () => {
     beforeEach(() => {
-        jest.setTimeout(60000000000000);
+        jest.setTimeout(60000);
         categoryRepositoryInMemory = new CategoryRepositoryInMemory();
         addressRepositoryInMemory = new AddressRepositoryInMemory();
         addressProvider = new AddressProvider();
@@ -97,20 +97,20 @@ describe("Create user", () => {
         ).rejects.toEqual(new AppError("Category doesn't exists!"));
     });
 
-    // it("Should not be able to create a user sending a non existing cep", async () => {
-    //     const { id: category_id } = await categoryRepositoryInMemory.create({
-    //         name: "category_test",
-    //         description: "description_test",
-    //     });
+    it("Should not be able to create a user sending a non existing cep", async () => {
+        const { id: category_id } = await categoryRepositoryInMemory.create({
+            name: "category_test",
+            description: "description_test",
+        });
 
-    //     await expect(
-    //         await createUserUseCase.execute({
-    //             name: "user_test",
-    //             cpf: "136.927.690-75",
-    //             cep: "non_existing_cep",
-    //             numero: 131,
-    //             category_id,
-    //         })
-    //     ).rejects.toEqual(new AppError("User already exists!"));
-    // });
+        await expect(
+            createUserUseCase.execute({
+                name: "user_test",
+                cpf: "136.927.690-75",
+                cep: "11111111",
+                numero: 131,
+                category_id,
+            })
+        ).rejects.toEqual(new AppError("Invalid CEP!"));
+    });
 });
--- src/shared/container/providers/addressProvider/implementations/AddressProvider.ts
+++ src/shared/container/providers/addressProvider/implementations/AddressProvider.ts
@@ -1,27 +1,33 @@
 import { consultarCep } from "correios-brasil/dist";
 
 import { Address } from "@modules/users/infra/typeorm/entities/Address";
+import { AppError } from "@shared/errors/AppError";
 
 import { IAddressProvider } from "../IAddressProvider";
 
 class AddressProvider implements IAddressProvider {
     async getAddress(cep: string, numero: number): Promise<Address> {
-        const cepData = await consultarCep(cep);
+        try {
+            const cepData = await consultarCep(cep);
 
-        if (cepData) {
-            const { logradouro, complemento, bairro, localidade, uf } = cepData;
+            if (cepData) {
+                const { logradouro, complemento, bairro, localidade, uf } =
+                    cepData;
 
-            const address: Address = {
-                cep,
-                logradouro,
-                complemento,
-                bairro,
-                localidade,
-                uf,
-                numero,
-            };
+                const address: Address = {
+                    cep,
+                    logradouro,
+                    complemento,
+                    bairro,
+                    localidade,
+                    uf,
+                    numero,
+                };
 
-            return address;
+                return address;
+            }
+        } catch {
+            throw new AppError("Invalid CEP!");
         }
 
         return null;
